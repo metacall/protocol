05a95c8d1ac10a4e5bdc5a6f8b7e9108cbca4345
AbhisheGoel <44609236+chotubhai@users.noreply.github.com>
2022-02-17 12:58:42 +0530

    /api-docs running in dev mode

--- src/apiDocs.ts
+++ src/apiDocs.ts
@@ -1,22 +1,26 @@
 import express from 'express';
 import swaggerUi, { JsonObject } from 'swagger-ui-express';
 import YAML from 'yamljs';
-const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
-const app: express.Application = express();
-const swaggerDocument: unknown = YAML.load('./swagger.yaml');
 
-const options = {
-	swaggerOptions: {
-		supportedSubmitMethods: []
-	}
-};
+if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'dev') {
+	console.log(process.env);
+	const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
+	const app: express.Application = express();
+	const swaggerDocument: unknown = YAML.load('./swagger.yaml');
 
-app.use(
-	'/api-docs',
-	swaggerUi.serve,
-	swaggerUi.setup(swaggerDocument as JsonObject, options)
-);
+	const options = {
+		swaggerOptions: {
+			supportedSubmitMethods: []
+		}
+	};
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-});
+	app.use(
+		'/api-docs',
+		swaggerUi.serve,
+		swaggerUi.setup(swaggerDocument as JsonObject, options)
+	);
+
+	app.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`);
+	});
+}
