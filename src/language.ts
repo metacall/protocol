af847b4ed35bbbab6aa1e5997f397a361a7ae655
Creatoon <yasharyan1099@gmail.com>
2022-07-04 15:15:54 +0530

    created displayNames map based on runner name

--- src/language.ts
+++ src/language.ts
@@ -16,6 +16,14 @@ interface Language {
 	runnerFilesRegexes: RegExp[]; // Regex for generating the runners list
 }
 
+const displayNameMap : Record<string, string> = {
+	'nodejs': 'NPM',
+	'python': 'PIP',
+	'ruby': 'GEM',
+	'csharp': 'NuGet'
+}
+
+
 export const Languages: Record<LanguageId, Language> = {
 	cs: {
 		tag: 'cs',
@@ -92,3 +100,5 @@ export const DisplayNameToLanguageId: Record<string, LanguageId> = Object.keys(
 		}),
 	{}
 );
+
+export const runnerDisplayName = (runner: string) : string => displayNameMap[runner] || 'Deploy' ;
