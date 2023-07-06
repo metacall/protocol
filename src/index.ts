92c543447b17a3e6b7a373c963a9f2e9716be2f5
durgesh077 <durgeshmaurya07@gmail.com>
2023-07-06 08:49:59 +0530

    added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("testing for reference error")
+export * from './deployment';
+export * from './language';
+export * from './login';
+export * from './package';
+export * from './plan';
+export * from './token';
+import metacallAPI from './protocol';
+export * from './protocol';
+export default metacallAPI;
